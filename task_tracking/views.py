7a5a6550dfa63dd0df445f273ca7ea27b86b8987
Illugha <miagreengal@gmail.com>
2025-10-04 17:31:09 +0300

    added log in through admin panel for adding tasks

--- task_tracking/views.py
+++ task_tracking/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from .models import Task
 from django.views.generic import ListView, DetailView, CreateView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from task_tracking.forms import TaskForm
+from task_tracking.mixins import UserIsOwnerMixin
+from django.http import HttpResponseRedirect
 
 # List of existing tasks
 class TaskListView(ListView):
@@ -16,8 +19,19 @@ class TaskDetailView(DetailView):
     context_object_name = 'task'
     template_name = 'tasks/task_detail.html'
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/task_form.html'
     success_url = reverse_lazy('task_tracking:task-list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class TaskCompleteView(LoginRequiredMixin, UserIsOwnerMixin, View):
+    def post(self, request, *args, **kwargs):
+        task = Task.objects.get(pk=kwargs['pk'])
+        task.status = 'completed'
+        task.save()
+        return HttpResponseRedirect(reverse_lazy('task_tracking:task-detail', kwargs={'pk': task.pk}))
