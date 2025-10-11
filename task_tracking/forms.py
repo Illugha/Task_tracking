edabae8113b05579aaaaa202d4da55a25716b418
Illugha <miagreengal@gmail.com>
2025-10-11 16:16:36 +0300

    added functional: filtering, update task, delete task, complete task (of course if its your task) :)

--- task_tracking/forms.py
+++ task_tracking/forms.py
@@ -5,3 +5,32 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'status', 'priority', 'deadline']
+
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+
+class TaskFilterForm(forms.Form):
+    STATUS_CHOICES = [
+        ('', 'All'),
+        ('todo', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Completed'),
+    ]
+    PRIORITY_CHOICES = [
+        ('', 'All'),
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    status = forms.ChoiceField(choices=STATUS_CHOICES, required=False, label='Status')
+    priority = forms.ChoiceField(choices=PRIORITY_CHOICES, required=False, label='Priority')
+    deadline_from = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}), label='Deadline From')
+    deadline_to = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}), label='Deadline To')
+
+    def __init__(self, *args, **kwargs):
+        super(TaskFilterForm, self).__init__(*args, **kwargs)
+        self.fields['status'].widget.attrs.update({'class': 'form-control'})
+        self.fields['priority'].widget.attrs.update({'class': 'form-control'})
--- task_tracking/views.py
+++ task_tracking/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from .models import Task
-from django.views.generic import ListView, DetailView, CreateView, View
+from django.views.generic import ListView, DetailView, CreateView, View, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from task_tracking.forms import TaskForm
+from task_tracking.forms import TaskForm, TaskFilterForm
 from task_tracking.mixins import UserIsOwnerMixin
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 
 # List of existing tasks
 class TaskListView(ListView):
@@ -13,6 +14,23 @@ class TaskListView(ListView):
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
     paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status = self.request.GET.get('status')
+        priority = self.request.GET.get('priority')
+
+        if status:
+            queryset = queryset.filter(status=status)
+        if priority:
+            queryset = queryset.filter(priority=priority)
+            
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TaskFilterForm(self.request.GET)
+        return context
 
 class TaskDetailView(DetailView):
     model = Task
@@ -28,10 +46,37 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+class TaskUpdateView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
+    model = Task
+    template_name = "tasks/task_update.html"
+    form_class = TaskForm
+    success_url = reverse_lazy("task_tracking:task-list")
     
-class TaskCompleteView(LoginRequiredMixin, UserIsOwnerMixin, View):
+class TaskDeleteView(LoginRequiredMixin, UserIsOwnerMixin, View):
+    template_name = "tasks/task_confirm_delete.html"
+    success_url = reverse_lazy("task_tracking:task-list")
+
+    def get_object(self):
+        task_id = self.kwargs.get('pk')
+        return get_object_or_404(Task, pk=task_id)
+
+    def get(self, request, *args, **kwargs):
+        task = self.get_object()
+        return render(request, self.template_name, {'object': task})
+
     def post(self, request, *args, **kwargs):
-        task = Task.objects.get(pk=kwargs['pk'])
-        task.status = 'completed'
+        task = self.get_object()
+        task.delete()
+        return redirect(self.success_url)
+
+class TaskCompleteView(LoginRequiredMixin, UserIsOwnerMixin, View):
+    def get_object(self):
+        task_id = self.kwargs.get('pk')
+        return get_object_or_404(Task, pk=task_id)
+
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.status = 'done'
         task.save()
-        return HttpResponseRedirect(reverse_lazy('task_tracking:task-detail', kwargs={'pk': task.pk}))
+        return HttpResponseRedirect(reverse_lazy('task_tracking:task-list'))
