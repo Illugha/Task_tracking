27291ae3fde1246e449e8b98cc1f068f638103d2
Illya Halych <miagreengal@gmail.com>
2025-10-24 21:09:49 +0300

    added new style of adding comments

--- task_tracking/urls.py
+++ task_tracking/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
     path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task-delete'),
     path('<int:pk>/complete/', views.TaskCompleteView.as_view(), name='task-complete'),
-    path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='task-comment'),
     path('delete-comment/<int:pk>/', views.CommentDeleteView.as_view(), name='comment-delete'),
     path('403/', views.custom_403_view.as_view(), name='custom-403'),
 ]
