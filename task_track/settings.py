3769d0be82bdc45baa517f983022a915a7bdd542
Illya Halych <miagreengal@gmail.com>
2025-11-01 14:25:44 +0200

    added styles to form and logining not through admin pannel

--- task_track/settings.py
+++ task_track/settings.py
@@ -116,14 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'task_tracking/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/admin/login/'
+LOGIN_URL = '/login/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'task_tracking:task-list'
+LOGOUT_REDIRECT_URL = 'task_tracking:logout'
--- task_tracking/urls.py
+++ task_tracking/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task-delete'),
     path('<int:pk>/complete/', views.TaskCompleteView.as_view(), name='task-complete'),
     path('delete-comment/<int:pk>/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/like/<int:pk>/', views.CommentLikeToggle.as_view(), name='comment-like-toggle'),
     path('403/', views.custom_403_view.as_view(), name='custom-403'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
 
 app_name = 'task_tracking'
